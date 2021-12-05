c21968177a70f8619a3618672289ba86e7fdaa2d
Johannes Blaschke <jpblaschke@lbl.gov>
2021-12-05 10:36:57 -0800

    clean up project structure a wee bit

--- PyGPU/data_type.cpp
+++ PyGPU/data_type.cpp
@@ -1,6 +1,39 @@
 #include <data_type.h>
 
 
+//----------------------------------------------------------------------------//
+// <data-type> <enum> <string identifiers>
+//  the first string identifier is the "canonical name" (i.e. what gets encoded)
+//  and the remaining string entries are used to generate aliases
+//
+DATA_TYPE(int16_t, Int16, "int16", "short")
+DATA_TYPE(int32_t, Int32, "int32", "int")
+DATA_TYPE(int64_t, Int64, "int64", "long")
+DATA_TYPE(uint16_t, UInt16, "uint16", "unsigned_short")
+DATA_TYPE(uint32_t, UInt32, "uint32", "unsigned", "unsigned_int")
+DATA_TYPE(uint64_t, UInt64, "uint64", "unsigned_long")
+DATA_TYPE(float, Float32, "float32", "float")
+DATA_TYPE(double, Float64, "float64", "double")
+
+
 int CudaError::as_int() const {
     return static_cast<int>(error_code);
+}
+
+
+void generate_enumeration(py::module & _mod) {
+    py::enum_<DataType> _dtype(_mod, "dtype", "Raw data types");
+    _dtype.export_values();
+    generate_enumeration<DataTypeSpecialization>(
+        _dtype,
+        std::make_index_sequence<DataTypesEnd>{}
+    );
+}
+
+
+void generate_datatype(py::module & _mod) {
+    generate_datatype<DataTypeSpecialization>(
+        _mod,
+        std::make_index_sequence<DataTypesEnd>{}
+    );
 }
--- PyGPU/data_type.h
+++ PyGPU/data_type.h
@@ -3,6 +3,8 @@
 
 #include <set>
 #include <cuda_hip_wrapper.h>
+#include <pybind11/pybind11.h>
+
 
 // TODO: these are from the PyKokkos source code -- and they need to be
 // documented
@@ -10,11 +12,13 @@
 #define FOLD_EXPRESSION(...)                                                   \
     ::consume_parameters(::std::initializer_list<int>{(__VA_ARGS__, 0)...})
 
+
 #define GET_FIRST_STRING(...)                                                  \
     static std::string _value = []() {                                         \
         return std::get<0>(std::make_tuple(__VA_ARGS__));                      \
     }();                                                                       \
     return _value
+
 
 #define GET_STRING_SET(...)                                                    \
     static auto _value = []() {                                                \
@@ -28,6 +32,7 @@
     }();                                                                       \
     return _value
 
+
 #define DATA_TYPE(TYPE, ENUM_ID, ...)                                          \
     template <>                                                                \
     struct DataTypeSpecialization<ENUM_ID> {                                   \
@@ -36,11 +41,14 @@
         static const auto & labels() { GET_STRING_SET(__VA_ARGS__); }          \
     };
 
+
 template <typename... Args>
 void consume_parameters(Args && ...) {}
 
+
 template <size_t data_type>
 struct DataTypeSpecialization;
+
 
 template <class T>
 class ptr_wrapper {
@@ -59,6 +67,7 @@ class ptr_wrapper {
         T * ptr;
 };
 
+
 struct CudaError {
 
     cudaError_t error_code;
@@ -67,5 +76,58 @@ struct CudaError {
     CudaError(int a_error) : error_code(static_cast<cudaError_t>(a_error)) {}
     int as_int() const;
 };
+
+
+namespace py = pybind11;
+
+
+enum DataType {
+    Int16 = 0,
+    Int32,
+    Int64,
+    UInt16,
+    UInt32,
+    UInt64,
+    Float32,
+    Float64,
+    DataTypesEnd
+};
+
+
+template <template <size_t> class SpecT, typename Tp, size_t ... Idx>
+void generate_enumeration(
+        py::enum_<Tp> & _enum, std::index_sequence<Idx ...>
+    ) {
+        auto _generate = [& _enum](const auto & _labels, Tp _idx) {
+            for (const auto & itr : _labels) {
+                assert(!itr.empty());
+                _enum.value(itr.c_str(), _idx);
+            }
+        };
+
+        FOLD_EXPRESSION(_generate(SpecT<Idx>::labels(), static_cast<Tp>(Idx)));
+}
+
+
+template <template <size_t> class SpecT, size_t ... DataIdx>
+void generate_datatype(py::module & _mod, std::index_sequence<DataIdx ...>) {
+    FOLD_EXPRESSION(
+        py::class_<ptr_wrapper<typename SpecT<DataIdx>::type>>(
+            _mod, ("ptr_wrapper_" + SpecT<DataIdx>::label()).c_str()
+        )
+        .def(py::init<>())
+        .def("create", & ptr_wrapper<typename SpecT<DataIdx>::type>::create)
+        .def("destroy", & ptr_wrapper<typename SpecT<DataIdx>::type>::destroy)
+        .def("__repr__",
+            [](const ptr_wrapper<typename SpecT<DataIdx>::type> & a) {
+                return "<ptr_wrapper<" +  SpecT<DataIdx>::label() + ">";
+            }
+        )
+    );
+}
+
+
+void generate_enumeration(py::module & _mod);
+void generate_datatype(py::module & _mod);
 
 #endif
