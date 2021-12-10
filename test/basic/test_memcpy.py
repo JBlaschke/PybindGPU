4d6e6e93f4282d7aae1dce466bf25644816f7d33
Johannes Blaschke <jpblaschke@lbl.gov>
2021-12-10 06:26:06 -0800

    clean up basic test

---
+++ test/basic/test_memcpy.py
@@ -0,0 +1,21 @@
+import numpy, PyGPU
+
+a = numpy.array([1., 2., 3., 4., 5.])
+print(a.dtype)
+
+# Alias a DeviceArray to the numpy array. Note that the suffix (_float64) needs
+# to match the numpy array's dtype. Basic wrapper types (like
+# DeviceArray_<dtype>) DON'T check the data type of buffers they are aliased to
+da = PyGPU.DeviceArray_float64(a)
+
+# Allocate memory on device (and check result)
+da.allocate()
+print(da.last_status())
+
+# Send data to device (and check result)
+da.to_device()
+print(da.last_status())
+
+# Send data to host (and check result)
+da.to_host()
+print(da.last_status())
