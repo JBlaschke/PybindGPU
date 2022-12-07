5c1d64c609e4492ee3160781684165e57b13310b
Johannes Blaschke <jpblaschke@lbl.gov>
2022-12-06 21:46:52 -0800

    typo

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ def make_extension():
         extra_compile_args={
             "gcc": ["-std=c++14", "-O3", "-shared", "-fPIC", "-DUSE_HIP"],
             "hipcc": ["-std=c++14", "-O3", "-fPIC", "-fgpu-rdc", "-DUSE_HIP",
-                      f"--amdgpu-target=${HIP_TARGET}"]
+                      f"--amdgpu-target={HIP_TARGET}"]
         }
 
     return Extension(
