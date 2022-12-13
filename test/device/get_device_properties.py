d7a1d257c7a66848c81dca389452d42a06ed91e7
Johannes Blaschke <jpblaschke@lbl.gov>
2022-12-12 22:09:02 -0800

    fix test

--- test/device/get_device_properties.py
+++ test/device/get_device_properties.py
@@ -3,10 +3,10 @@
 
 import PybindGPU
 
-n_device, err = py_device_properties.cudaGetDeviceCount()
+n_device, err = PybindGPU.cudaGetDeviceCount()
 
 for i in range(n_device):
-    prop = py_device_properties.cudaDeviceProp(i)
+    prop = PybindGPU.cudaDeviceProp(i)
     print(f"Device {i}:")
     print(f" | name = {prop.name()}")
     print(f" | uuid = {prop.uuid()}")
