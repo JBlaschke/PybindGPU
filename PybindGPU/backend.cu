8966284f0a35ded82a26679cbfbaf5c3f15121b0
darrenjhsu <hsudj@ornl.gov>
2022-12-20 15:16:44 -0500

    Edit test, backend ifndef, and make clean more thorough

--- PybindGPU/backend.cu
+++ PybindGPU/backend.cu
@@ -154,6 +154,7 @@ PYBIND11_MODULE(backend, m) {
                 return s;
             }
         )
+#ifndef USE_HIP
         .def("uuid",
             [](DeviceProperties & a) {
                 std::string s = mem_to_string(
@@ -162,6 +163,7 @@ PYBIND11_MODULE(backend, m) {
                 return s;
             }
         )
+#endif
         .def("pciBusID",
             [](DeviceProperties & a) {
                 return a.get()->pciBusID;
@@ -193,7 +195,7 @@ PYBIND11_MODULE(backend, m) {
         );
 
     m.attr("major_version")   = py::int_(0);
-    m.attr("minor_version")   = py::int_(2);
+    m.attr("minor_version")   = py::int_(1);
     m.attr("release_version") = py::int_(0);
 
     // Let the user know that this backend has been compiled _with_ CUDA support
--- clean.sh
+++ clean.sh
@@ -2,3 +2,4 @@
 
 _dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 rm -rf ${_dir}/PybindGPU.egg-info ${_dir}/build ${_dir}/dist
+rm -rf ${_dir}/PybindGPU/backend.cpython* ${_dir}/PybindGPU/__pycache__
